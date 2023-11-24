ed8378f229c090f0bcdb4625bd11ba99e3296bf6
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-11-24 13:19:15 +0530

    create a endpoint to retrive posts

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+posts = []
+
 @app.get("/")
 def root():
     return {"name": "simple api", "msg": "hello", "version": "v0.1.0-alpha"}
+
+@app.get("/posts")
+def get_posts():
+    return {"posts": posts}
--- runapi.py
+++ runapi.py
@@ -1,14 +1,11 @@
 import subprocess
-import os
 
 def run_fastapi():
     # Specify the command to run FastAPI using Uvicorn
-    command = "uvicorn main:app"
+    command = "uvicorn app.main:app"
 
     # Run the command using subprocess
     subprocess.run(command, shell=True)
 
 if __name__ == "__main__":
-    # Run the FastAPI app in the 'app' directory
-    os.chdir('app')
     run_fastapi()
