018f5ce3dd5a21c4312afe148c579a1ddeef5a5f
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-11-24 13:49:27 +0530

    add endpoint to get post with specfic id

--- app/main.py
+++ app/main.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-posts = []
+posts = [
+    {
+        "id": 1,
+        "title": "my first post",
+        "content": "placeholder content....",
+    },
+    {
+        "id": 2,
+        "title": "my second post",
+        "content": "placeholder demo content....",
+    }
+]
 
 @app.get("/")
 def root():
@@ -11,3 +22,10 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"posts": posts}
+
+@app.get("/posts/{post_id}")
+def get_post_with_id(post_id: int):
+    for item in posts:
+        if item["id"] == post_id:
+            return item
+    return {"data": f"Post With Id {post_id} Not Found!!"}
