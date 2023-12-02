a6bbc03aee2f61d061436ef636e0b6106dbdf164
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-12-02 21:54:39 +0530

    create a basic database schema

--- Database/database.py
+++ Database/database.py
@@ -33,8 +33,21 @@ def create_database():
 
 def init():
     create_database()
-    querys = [
-        "CREATE TABLE IF NOT EXISTS posts (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, description VARCHAR(500))",
+    queries = [
+    "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) UNIQUE, password VARCHAR(500), bio TEXT, location VARCHAR(255), other_details TEXT)",
+    
+    "CREATE TABLE IF NOT EXISTS posts (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, description VARCHAR(500), published_by INT, FOREIGN KEY (published_by) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE)",
+    
+    "CREATE TABLE IF NOT EXISTS comments (id INT AUTO_INCREMENT PRIMARY KEY, post_id INT, user_id INT, comment_text VARCHAR(500), FOREIGN KEY (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE, FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE)",
+    
+    "CREATE TABLE IF NOT EXISTS likes (id INT AUTO_INCREMENT PRIMARY KEY, post_id INT, user_id INT, FOREIGN KEY (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE, FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE)",
+    
+    "CREATE TABLE IF NOT EXISTS followers (id INT AUTO_INCREMENT PRIMARY KEY, follower_id INT, following_id INT, FOREIGN KEY (follower_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE, FOREIGN KEY (following_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE)",
+    
+    "CREATE TABLE IF NOT EXISTS media (media_id INT AUTO_INCREMENT PRIMARY KEY, post_id INT, media_type ENUM('image', 'video'), media_data LONGBLOB, FOREIGN KEY (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE)",
+    
+    "CREATE TABLE IF NOT EXISTS messages (message_id INT AUTO_INCREMENT PRIMARY KEY, sender_id INT, receiver_id INT, message_text TEXT, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (sender_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE, FOREIGN KEY (receiver_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE)"
     ]
+
     for query in querys:
         execute_query(query)
